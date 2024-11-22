af9f8cf50a17f7c1a8065fed143b185f4eacdd41
Thomas Wrigley <thomas@thomaswrigley.com>
2024-11-22 06:21:17 +0000

    Clearing out tutorial files

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++
@@ -1,6 +0,0 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-
-from dbt_tutorial_jaffle_shop.customers
--- models/stg_orders.sql
+++
@@ -1,7 +0,0 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-
-from `utility-replica-441110-u8`.dbt_tutorial_jaffle_shop.orders
